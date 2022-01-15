8ff8c89357a72e40350c66f4a4c9fdf7a46fb21b
BetoSalcido <43348673+betosalcidoetips@users.noreply.github.com>
2022-01-15 10:09:39 -0700

    Initial commit

---
+++ capacitacion_2/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  capacitacion_2
+//
+//  Created by Beto Salcido on 15/01/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ capacitacion_2/TabBarController.swift
@@ -0,0 +1,50 @@
+//
+//  TabBarController.swift
+//  Kelder
+//
+//  Created by Beto Salcido on 15/04/21.
+//  Copyright © 2021 BetoSalcido. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tabBar.isTranslucent = false
+        self.delegate = self
+
+        setUpViewController()
+    }
+}
+
+// MARK: - Private Methods
+private extension TabBarController {
+    
+    func setUpViewController() {
+        let homeViewController = UIStoryboard(name: "Home", bundle: nil).instantiateViewController(withIdentifier: "HomeViewController") as! HomeViewController
+        
+        
+        viewControllers = [
+            createTabController(for: homeViewController, title: "Inicio", selected: UIImage(named: "icon_ic_kelder")!),
+        ]
+    }
+    
+    func createTabController(for rootViewController: UIViewController, title: String, selected: UIImage) -> UINavigationController {
+        let navController = UINavigationController(rootViewController: rootViewController)
+        navController.navigationBar.barTintColor = .white
+        navController.tabBarItem.image = selected
+        navController.tabBarItem.title = title
+        navController.navigationBar.barStyle = .default
+        return navController
+    }
+}
+
+// MARK: - UITabBarControllerDelegate
+extension TabBarController: UITabBarControllerDelegate {
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+    }
+}
+    
