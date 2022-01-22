8e6cd717ff44cec4b0e595103e4d7150ce1e1889
BetoSalcido <43348673+betosalcidoetips@users.noreply.github.com>
2022-01-22 13:18:58 -0700

    Push to collectionVIew added

--- capacitacion_2/HomeViewController.swift
+++ capacitacion_2/HomeViewController.swift
@@ -12,6 +12,26 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var productImage: UIImageView!
     @IBOutlet weak var categoriesButton: UIButton!
     
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        // Variable para poner el titulo a la pantalla
+        self.title = "Inicio"
+    }
+}
+
+// MARK: - Actions
+extension HomeViewController {
+    @IBAction func didTapCollectionViewButton(_ sender: Any) {
+        guard let viewController = UIStoryboard(name: "CollectionView", bundle: nil).instantiateViewController(identifier: "CollectionViewController") as? CollectionViewController else {
+            print("Error")
+            return
+        }
+        
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+    
     @IBAction func didTapCategoriesButton(_ sender: Any) {
         guard let viewController = UIStoryboard(name: "Home", bundle: nil).instantiateViewController(identifier: "AnimalViewController") as? AnimalViewController else {
             print("Error")
@@ -34,12 +54,6 @@ class HomeViewController: UIViewController {
         
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        // Variable para poner el titulo a la pantalla
-        self.title = "Inicio"
-    }
 }
 
 extension HomeViewController: AnimalDelegate {
