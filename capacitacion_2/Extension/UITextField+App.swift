093cb9f7c3c96223bc357d0812c634d761c6dc99
BetoSalcido <43348673+betosalcidoetips@users.noreply.github.com>
2022-02-19 10:10:26 -0700

    missing file

---
+++ capacitacion_2/Extension/UITextField+App.swift
@@ -0,0 +1,34 @@
+//
+//  UITextField+App.swift
+//  capacitacion_2
+//
+//  Created by Beto Salcido on 19/02/22.
+//
+
+import Foundation
+
+extension UITextField {
+    enum Position {
+        case left
+        case right
+    }
+    
+    func setIcon(with image: UIImage, position: Position) {
+        let iconView = UIImageView(frame: CGRect(x: 0, y: 5, width: 20, height: 20))
+        iconView.image = image
+        
+        let iconContainerView: UIView = UIView(frame:CGRect(x: 0, y: 0, width: 30, height: 30))
+        iconContainerView.addSubview(iconView)
+        
+        if position == .left {
+            leftView = iconContainerView
+            leftViewMode = .always
+            
+        } else {
+            rightView = iconContainerView
+            rightViewMode = .always
+        }
+        
+
+    }
+}
